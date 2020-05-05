f0b93aa855de6d583f82246f5e770a5a194c0b2b
Lucas Löffel <lucas@loeffel.io>
2020-05-05 12:50:02 +0200

    add whitelines

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
 import Saas from '@go-saas/go-saas-ui/src/saas';
+
 // packages
 import Router from "@go-saas/go-saas-ui/src/packages/router/basic/router";
 import Axios from "@go-saas/go-saas-ui/src/packages/http/axios/axios";
@@ -6,8 +7,10 @@ import I18n from "@go-saas/go-saas-ui/src/packages/i18n/basic/i18n";
 import LocalStorage from "@go-saas/go-saas-ui/src/packages/storage/local-storage/local-storage";
 import Event from "@go-saas/go-saas-ui/src/packages/event/basic/event";
 import Security from "@go-saas/go-saas-ui/src/packages/security/basic/security";
+
 // components
 import Master from "@go-saas/go-saas-ui/src/components/master/Master.vue";
+
 // scss
 import './scss/app.scss'
 
