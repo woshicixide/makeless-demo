507d9558298b3e1084215934e102f67ebd1d64dc
Lucas Löffel <lucas@loeffel.io>
2020-06-02 12:54:11 +0200

    remove master

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,9 +9,6 @@ import LocalStorage from "@go-saas/go-saas-ui/src/packages/storage/local-storage
 import Event from "@go-saas/go-saas-ui/src/packages/event/basic/event";
 import Security from "@go-saas/go-saas-ui/src/packages/security/basic/security";
 
-// components
-import Master from "@go-saas/go-saas-ui/src/components/layouts/Master.vue";
-
 // scss
 import './scss/app.scss'
 
@@ -26,6 +23,6 @@ const storage = new LocalStorage();
 const event = new Event(config.getConfiguration().getHost());
 const security = new Security(router, http, event, storage);
 
-new Saas(config, Master, router, http, i18n, event, security)
+new Saas(config, router, http, i18n, event, security)
     .init()
     .run();
