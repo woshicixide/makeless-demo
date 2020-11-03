f95c15211a1dcc3e7f341cc1a94a52eedf531d9c
Lucas Löffel <lucas@loeffel.io>
2020-11-03 12:43:48 +0100

    update makeless

--- frontend/src/shims-makeless.d.ts
+++ frontend/src/shims-makeless.d.ts
@@ -3,8 +3,5 @@ import Makeless from '@makeless/makeless-ui/src/makeless';
 declare module 'vue/types/vue' {
     interface Vue {
         $makeless: Makeless;
-        userLoaded: boolean;
-
-        onUserLoaded(): void
     }
 }
